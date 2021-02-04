3c3d7128b3196aa2db0c190aca58be70e52ff9c3
2017398956 <2017398956@qq.com>
2021-02-04 14:51:41 +0800

    myShadow

--- projects/sample/source/sample-host/src/main/java/com/tencent/shadow/sample/host/HostApplication.java
+++ projects/sample/source/sample-host/src/main/java/com/tencent/shadow/sample/host/HostApplication.java
@@ -51,7 +51,8 @@ public class HostApplication extends Application {
 
         if (isProcess(this, ":plugin")) {
             //在全动态架构中，Activity组件没有打包在宿主而是位于被动态加载的runtime，
-            //为了防止插件crash后，系统自动恢复crash前的Activity组件，此时由于没有加载runtime而发生classNotFound异常，导致二次crash
+            //为了防止插件crash后，系统自动恢复crash前的Activity组件，此时由于没有加载runtime而发生classNotFound异常，
+            // 导致二次crash
             //因此这里恢复加载上一次的runtime
             DynamicRuntime.recoveryRuntime(this);
         }
